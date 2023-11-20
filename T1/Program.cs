a165614eb9e699b672722118ff7dec329ad300ba
Elena Serebrennikova <eserebrennikovaa@mail.ru>
2023-11-21 00:37:10 +0300

    add new pr

---
+++ T1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 1: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа
+// в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+Console.Clear();
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int NaturalNumber(int n, int m)
+{
+  if (n == m) return n;
+  else Console.Write($"{NaturalNumber(n, m + 1)}, ");
+  return m;
+}
+
+int n = Prompt("Input N: ");
+if (n < 1)
+{
+  Console.WriteLine("Ввдите положительное число!");
+  return;
+}
+Console.WriteLine(NaturalNumber(n, 1));
---
+++ Ta3/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+ Console.Clear();
+ int[] CreateArr(int number)
+{
+  Console.Write("Введите первое число: ");
+  int start = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Выедите второе число: ");
+  int stop = Convert.ToInt32(Console.ReadLine());
+
+  int[] Arr = new int[number];
+  for (int i = 0; i < Arr.Length; i++)
+  {
+    Arr[i] = new Random().Next(start, stop + 1);
+  }
+  return Arr;
+}
+
+int[] ReversArr(int[] Array)
+{
+  for (int i = 0, j = Array.Length - 1; i < Array.Length / 2; i++, j--)
+  {
+    int tmp = Array[j];
+    Array[j] = Array[i];
+    Array[i] = tmp;
+  }
+  return Array;
+}
+
+int[] res = CreateArr(5);
+Console.WriteLine(String.Join(" ", res));
+Console.WriteLine(String.Join(" ", ReversArr(res)));
